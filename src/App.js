fe5e7bc797d8b3bcf9dfa71c4dfb67962f99ef59
Tim Cullingham <45502642+TimCullingham@users.noreply.github.com>
2020-02-08 19:35:27 -0800

    starting working on the app based o a tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import MovieRow from './MovieRow.js';
+import $ from 'jquery'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+    //console.log("This is my initializer")
+
+    //   const movies = [
+    //     {id: 0, poster_src: "", title:"Yadada", overview:"Dingidin"},
+    //     {id: 1, poster_src: "", title:"Yada", overview:"Dingi"}
+    //   ]
+
+    //   let movieRows = []
+    //   movies.forEach((movie) => {
+    //     console.log(movie.title)
+    //     const movieRow = <MovieRow movie={movie} />
+    //     movieRows.push(movieRow)
+    //   })
+
+    //   this.state = {rows: movieRows}
+  this.performSearch("avengers")
+  }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "url&query" +
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        //console.log(searchResults)
+        const results = searchResults.results
+        //console.log(results[0])
+
+
+        let movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "" + movie.poster_path
+          //console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
+
+    searchChangeHandler(event) {
+      console.log(event.target.value)
+      const searchTerm = event.target.value
+      this.performSearch(searchTerm)
+    }
+
+    render()  {
+      return (
+      <div>
+        
+        <table class="header">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app-logo" className="logo" src=""/>
+              </td>
+              <td>
+                <h3>Movie DB Search</h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input onChange={this.searchChangeHandler.bind(this)} className="searchbar" placeholder="Enter Search Term"/>
+
+        {this.state.rows}
+        
+      </div>
+    );
+  }
 }
-
 export default App;
